b7ff829b41b38bdd48aa3dd6968f327e88d894de
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-12-05 17:27:54 +0100

    Completed day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -5,7 +5,10 @@ import click
 @click.command()
 @click.option('--file', default='input.txt', help='Input file path')
 def run(file):
-    position = [0, 0]
+    santas_position = {
+        's': [0, 0],
+        'r': [0, 0],
+    }
     movements = {
         '>': [1, 0],
         '<': [-1, 0],
@@ -13,8 +16,9 @@ def run(file):
         'v': [0, 1],
     }
     houses = [
-        position,
+        santas_position['s'],
     ]
+    active_santa = 's'
     with click.open_file(file) as instructions:
         while True:
             where = instructions.read(1)
@@ -22,9 +26,14 @@ def run(file):
                 click.echo('We just visited {} houses'.format(len(houses)))
                 break
             if where in movements:
+                position = santas_position[active_santa]
                 position = [sum(i) for i in zip(*[movements[where], position])]
                 if position not in houses:
                     houses.append(position)
+                santas_position[active_santa] = position
+                santas = santas_position.keys()
+                santas.remove(active_santa)
+                active_santa = santas[0]
 
 
 if __name__ == '__main__':
