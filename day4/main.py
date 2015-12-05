c4f639e2fd9ebbcf7fc233d8f2c76d3baa025219
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-12-05 18:44:12 +0100

    Completed day 4 part 1

---
+++ day4/main.py
@@ -0,0 +1,23 @@
+#!python
+
+import click
+import hashlib
+
+@click.command()
+@click.option('--key', default='ckczppom', help='Input key')
+def run(key):
+    position = 1
+    while md5(key+str(position)).startswith('0'*5) == False:
+        position += 1
+    click.echo('Code is {}'.format(str(position)))
+
+
+def md5(key):
+    m = hashlib.md5()
+    m.update(key)
+
+    return m.hexdigest()
+
+
+if __name__ == '__main__':
+    run()
