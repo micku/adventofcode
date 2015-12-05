1b4f33f2c9dd1da441f0dd440538c91b506251c5
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-12-05 14:38:11 +0100

    Finished step two

--- day1/main.py
+++ day1/main.py
@@ -10,14 +10,22 @@ def run(file):
         '(': 1,
         ')': -1,
     }
+    index = 0
+    basement_position = 0
     with click.open_file(file) as instructions:
         while True:
             where = instructions.read(1)
             if not where:
+                click.echo('I was at -1 after {} steps...'.format(basement_position))
                 click.echo('{} floor!'.format(floor))
                 break
+            index += 1
             if where in directions:
                 floor += directions[where]
+            if not basement_position \
+                and floor < 0:
+                basement_position = index
+
 
 if __name__ == '__main__':
     run()
