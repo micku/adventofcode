acd48045f26405186ad5c395a73d564ea405adbe
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-12-11 11:36:53 +0100

    Day 8 part 2

--- day8/main.py
+++ day8/main.py
@@ -7,12 +7,18 @@ import re
 @click.option('--file', default='day8/input.txt', help='Input file path')
 def run(file):
     tot = 0
+    tot2 = 0
     with click.open_file(file) as strings:
         for row in strings:
             row = row.rstrip()
-            tot += len(row) - len(eval(row))
+            code_len = len(row)
+            memory_len = len(eval(row))
+            encoded_len = len(row.replace('\\', '\\\\').replace('"', '\\"')) + 2
+            tot += code_len - memory_len
+            tot2 += encoded_len - code_len
 
     click.echo('Total is {}'.format(tot))
+    click.echo('Total 2 is {}'.format(tot2))
 
 
 if __name__ == '__main__':
