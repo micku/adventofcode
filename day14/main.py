a5bcaa1a0bbcf6a6cdb25186a6972c023bf409d0
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-12-17 17:32:05 +0100

    Day 14 part 2

--- day14/main.py
+++ day14/main.py
@@ -17,6 +17,8 @@ def run(file):
                 int(data.group(2)),
                 int(data.group(3)),
                 int(data.group(4)),
+                0,
+                0,
             ]
     max_time = 2503
     totals = []
@@ -27,6 +29,17 @@ def run(file):
         )
 
     click.echo('Part 1 Total is {}'.format(max(totals)))
+
+    m = 0
+    for i in range(max_time):
+        for k, v in caribou_list.iteritems():
+            v[4] += 1 if v[3] == m and m > 0 else 0
+            v[3] += v[0] if i % (v[1] + v[2]) < v[1] else 0
+        m = max(zip(*caribou_list.values())[3])
+
+    tot2 = max(zip(*caribou_list.values())[4])
+
+    click.echo('Part 2 Total is {}'.format(tot2))
 
 if __name__ == '__main__':
     run()
