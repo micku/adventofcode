ceeddc3346560eed06166808ffb8d30d9582e92b
Michele Gargiulo <michele@grg.io>
2016-12-02 17:11:18 +0100

    Solves part 1

--- 2016/day1/main.py
+++ 2016/day1/main.py
@@ -7,16 +7,20 @@ import click
         type=click.File(),
         default='input.txt')
 def run(f):
-    floor = 0
-    directions = {
-        '(': 1,
-        ')': -1,
-    }
-    index = 0
-    basement_position = 0
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+    direction = 0
+    position = (0, 0)
 
     route = f.read().rstrip().split(', ')
-    print(route)
+    for instruction in route:
+        direction += 1 if instruction[0] == 'R' else -1
+        distance = int(instruction[1:])
+        position = (
+            position[0] + (directions[direction%4][0] * distance),
+            position[1] + (directions[direction%4][1] * distance)
+        )
+    print(sum([abs(x) for x in position]))
 
 
 if __name__ == '__main__':
