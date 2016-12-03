b9938a1d4a6b6f93875838ea29428d3848072183
Michele Gargiulo <michele@grg.io>
2016-12-03 11:34:04 +0100

    Day 3 part 2

--- 2016/day3/main.py
+++ 2016/day3/main.py
@@ -20,8 +20,26 @@ def run(part, f):
                 int(triangle[10:])]
             possible += 1 \
                 if all([True if sum(combination[:2]) > combination[2] else False 
-                    for combination in itertools.permutations(sides, 3)]) \
+                    for combination in itertools.permutations(sides)]) \
                 else 0
+
+    if part == '2':
+        idx = 0
+        sides = []
+        for triangle in f:
+            idx += 1
+            sides.append([
+                int(triangle[:5]),
+                int(triangle[5:10]),
+                int(triangle[10:])])
+            if idx%3 == 0:
+                z = zip(*sides[idx-3:idx])
+                for t in z:
+                    possible += 1 \
+                        if all([True if sum(combination[:2]) > combination[2] else False 
+                            for combination in itertools.permutations(t)]) \
+                        else 0
+
     click.echo(possible)
 
 
