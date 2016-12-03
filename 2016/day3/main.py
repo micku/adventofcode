588831f80f956aba668442a7192395c93c508b15
Michele Gargiulo <michele@grg.io>
2016-12-03 11:19:17 +0100

    Day 3 part 1

---
+++ 2016/day3/main.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+import click
+import operator
+import itertools
+
+@click.command()
+@click.argument('part',
+        default='1')
+@click.argument('f',
+        type=click.File(),
+        default='input.txt')
+def run(part, f):
+    possible = 0
+    if part == '1':
+        for triangle in f:
+            sides = [
+                int(triangle[:5]),
+                int(triangle[5:10]),
+                int(triangle[10:])]
+            possible += 1 \
+                if all([True if sum(combination[:2]) > combination[2] else False 
+                    for combination in itertools.permutations(sides, 3)]) \
+                else 0
+    click.echo(possible)
+
+
+if __name__ == '__main__':
+    run()
