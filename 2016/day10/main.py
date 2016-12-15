e09450d0d0863552495e2b2cfe8b98ba7df7051d
Michele Gargiulo <michele@grg.io>
2016-12-15 10:57:19 +0100

    Very ugly and lazy day 10 part 2

--- 2016/day10/main.py
+++ 2016/day10/main.py
@@ -13,17 +13,15 @@ import re
 def run(part, f):
     pending_bots = []
     ready_bots = []
+    outputs = []
 
-    if part == '1':
-        solve(f, pending_bots, ready_bots)
-
-    if part == '2':
-        pass
-
-    click.echo()
+    pending_bots, ready_bots, outputs = solve(f, pending_bots, ready_bots, outputs)
+    
+    zot = [x['values'][0] for x in outputs if x['id'] in [0, 1, 2]]
+    click.echo(zot[0]*zot[1]*zot[2])
 
 
-def solve(instructions, pending_bots, ready_bots):
+def solve(instructions, pending_bots, ready_bots, outputs):
     def parse(instruction):
         who, low, high = map(int,re.findall(r'-?\d+', instruction))
         return {
@@ -32,39 +30,53 @@ def solve(instructions, pending_bots, ready_bots):
             'max': high
         }
 
-    def assign(bot_id, val):
-        is_pending = filter(lambda x: x['id'] == bot_id, pending_bots)
-        if is_pending:
-            bot = is_pending[0]
-            pending_bots.remove(bot)
-            bot['values'].append(val)
-            ready_bots.append(bot)
-            if set(bot['values']).issuperset([17, 61]):
-                print bot['id']
+    def assign(t_id, val, is_bot):
+        if is_bot:
+            is_pending = filter(lambda x: x['id'] == t_id, pending_bots)
+            if is_pending:
+                bot = is_pending[0]
+                pending_bots.remove(bot)
+                bot['values'].append(val)
+                ready_bots.append(bot)
+                if set(bot['values']).issuperset([17, 61]):
+                    print bot['id']
+            else:
+                bot = {
+                    'id': t_id,
+                    'values': [val, ]
+                }
+                pending_bots.append(bot)
+            return bot
         else:
-            bot = {
-                'id': bot_id,
-                'values': [val, ]
-            }
-            pending_bots.append(bot)
-        return bot
+            is_pending = filter(lambda x: x['id'] == t_id, outputs)
+            if is_pending:
+                output = is_pending[0]
+                is_pending[0]['values'].append(val)
+            else:
+                output = {
+                    'id': t_id,
+                    'values': [val, ]
+                }
+                outputs.append(output)
+            return output
 
     next_instructions = []
     for instruction in instructions:
         if instruction.startswith('value'):
             val, who = map(int,re.findall(r'-?\d+', instruction))
-            assign(who, val)
+            assign(who, val, True)
         else:
             i = parse(instruction)
             is_ready = filter(lambda x: x['id'] == i['id'], ready_bots)
             if is_ready:
-                assign(i['min'], min(is_ready[0]['values']))
-                assign(i['max'], max(is_ready[0]['values']))
+                bot_out = re.findall(r' (bot|output)', instruction)
+                assign(i['min'], min(is_ready[0]['values']), bot_out[0] == 'bot')
+                assign(i['max'], max(is_ready[0]['values']), bot_out[1] == 'bot')
             else:
                 next_instructions.append(instruction)
-    return solve(next_instructions, pending_bots, ready_bots) \
+    return solve(next_instructions, pending_bots, ready_bots, outputs) \
             if next_instructions \
-            else pending_bots, ready_bots
+            else pending_bots, ready_bots, outputs
 
 
 
