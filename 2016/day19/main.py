ccf35ba000a4790b66310d98262f96c4dcf04805
Michele Gargiulo <michele@grg.io>
2017-11-30 11:39:03 +0100

    Uhm... Not sure why I left it here for a year...

--- 2016/day19/main.py
+++ 2016/day19/main.py
@@ -1,25 +1,25 @@
 #! /usr/bin/env python
 
 import click
-from collections import deque
+from collections import OrderedDict
 
 @click.command()
 @click.argument('f',
         type=click.File(),
         default='input.txt')
 def run(f):
-    elves = {}
-    s = deque(range(int(f.read().strip())))
-    for e in s:
+    elves = OrderedDict()
+    for e in range(int(f.read().strip())):
         elves[e] = 1
 
-    while len(s) > 1:
-        elf = s.popleft()
-        stolen_elf = s.popleft()
-        elves[elf] += elves[stolen_elf]
-        s.append(elf)
+    while len(elves.keys()) > 1:
+        elf = elves.popitem(last=False)
+        stolen_elf = elves.popitem(last=False)
+        elves.update({elf[0]: elf[1]+stolen_elf[1]})
+        elves.move_to_end(elf[0], last=True)
 
-    click.echo("Part 1: {}".format(s[0]+1))
+    winner = elves.popitem()
+    click.echo("Part 1: {}".format(winner[0]+1))
 
 
 if __name__ == '__main__':
